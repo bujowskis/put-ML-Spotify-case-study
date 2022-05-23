bc1cd96945b7c4a8a95dc1e95b44a0f5cede73e8
bujowskis <s.bujowski@gmail.com>
2022-05-23 21:25:04 +0200

    add track handling by uri

--- spotify-puller.py
+++ spotify-puller.py
@@ -3,9 +3,10 @@
 
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
+import pandas as pd
 
 
-def get_all_playlist_items(sp: spotipy.Spotify, playlist_id):
+def get_all_playlist_items(sp: spotipy.Spotify, playlist_id) -> list:
     """
     Pulls all songs from the given playlist, even if it exceeds the limit of 100 tracks
     :param sp: Spotify API client
@@ -21,9 +22,42 @@ def get_all_playlist_items(sp: spotipy.Spotify, playlist_id):
     return tracks
 
 
+def extract_tracks_uri(tracks_items: list) -> list:
+    """
+    Extracts tracks uri from the list of all item info of songs
+    :param tracks_items: list of all item info of all songs
+    :return: list of tracks uri
+    """
+    return [item['track']['uri'] for item in tracks_items]
+
+
+def make_tracks_data(sp: spotipy.Spotify, tracks_uri: list, liked: bool = None):
+    """
+    Creates pandas dataframe containing data useful for classification
+
+    If given, adds on boolean `liked` attribute
+    :param sp: Spotify API client
+    :param tracks_uri: list of tracks uri
+    :param liked: optional boolean attribute applied to all tracks (not used by default)
+    :return: pandas dataframe with dataset for the classification
+    """
+    ...
+
+
 if __name__ == "__main__":
     sp = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
-    like_playlist = "29e9lxxlte7IE7dwrSHhKP"  # TODO - change
-    long_playlist = "6IKQrtMc4c00YzONcUt7QH"
+    example_playlist = "29e9lxxlte7IE7dwrSHhKP"
 
-    print(get_all_playlist_items(sp, like_playlist)[0])
+    tracks_items = get_all_playlist_items(sp, example_playlist)
+    tracks_uri = extract_tracks_uri(tracks_items)
+
+    # ***** *** NOTE - interesting to potentially get into later, but for now
+    # aa = sp.audio_analysis(tracks_uri[0])
+    # print(aa['meta'])  # metadata regarding the machine analysis was run on and software
+    # print(aa['track'])  # seems to have some valuable things (?)
+    # print(aa['bars'])  # rather not
+    # print(aa['beats'])  # as above
+    # print(aa['sections'])  # interesting, but rather too complicated
+    # print(aa['segments'])  # as the above
+    # print(aa['tatums'])  # as bars or beats
+
