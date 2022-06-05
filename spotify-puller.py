4f01ada6e650e6776591320b5d7b468f864a64c1
sBujowski <s.bujowski@gmail.com>
2022-06-06 01:01:54 +0200

    implement playlists pulling

--- spotify-puller.py
+++ spotify-puller.py
@@ -5,6 +5,9 @@ import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 import pandas as pd
 
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
+
 
 def get_all_playlist_items(sp: spotipy.Spotify, playlist_id) -> list:
     """
@@ -22,36 +25,87 @@ def get_all_playlist_items(sp: spotipy.Spotify, playlist_id) -> list:
     return tracks
 
 
-def extract_tracks_uri(tracks_items: list) -> list:
-    """
-    Extracts tracks uri from the list of all item info of songs
-    :param tracks_items: list of all item info of all songs
-    :return: list of tracks uri
-    """
-    return [item['track']['uri'] for item in tracks_items]
-
-
-def make_tracks_data(sp: spotipy.Spotify, tracks_uri: list, liked: bool = None):
+def make_tracks_data(sp: spotipy.Spotify, playlist, liked: bool = None) -> pd.DataFrame:
     """
     Creates pandas dataframe containing data useful for classification
 
     If given, adds on boolean `liked` attribute
+
     :param sp: Spotify API client
-    :param tracks_uri: list of tracks uri
+    :param playlist: uri of the playlist
     :param liked: optional boolean attribute applied to all tracks (not used by default)
     :return: pandas dataframe with dataset for the classification
     """
-    ...
+    tracks_items = get_all_playlist_items(sp, example_playlist)
+
+    df = pd.DataFrame(columns=[
+        # ***** *** meta
+        'name', 'artists', 'uri',   # easy access to the song
+        # ***** *** functional
+        'acousticness',             # [0.0, 1.0]            - (not acoustic - acoustic)
+        'danceability',             # [0.0, 1.0]            - (not for dancing - suitable for dance)
+        'energy',                   # [0.0, 1.0]            - (not energetic, most energetic)
+        'instrumentalness',         # [0.0, 1.0]            - (lyrical, instrumental)
+        'key',                      # [0, 1, (...), 11]     - key according to standard Pitch Class notation
+                                    #                         https://en.wikipedia.org/wiki/Pitch_class
+        'liveness',                 # [0.0, 1.0]            - (studio recording, live performance)
+        'loudness',                 # [-60, 0]              - (quiet, loud)
+        'mode',                     # binary                - 0 (minor) or 1 (major)
+        'speechiness',              # [0.0, 1.0]            - (no speech, speech-intensive)
+        'tempo',                    # [0, inf)              - overall estimated tempo in BPM
+        'time_signature',           # [0, 1, (...),  inf)   - no. of beats in each bar TODO - inf?
+        'valence'                   # [0.0, 1.0]            - (sounds negative, sounds positive)
+    ])
+
+    tracks = [ti['track'] for ti in tracks_items]
+    for track in tracks:
+        track_data = dict()
+
+        # ***** *** meta
+        track_data['name'] = track['name']
+        artists = list()
+        for artist in track['artists']:
+            artists.append(artist['name'])
+        track_data['artists'] = artists  # track['artists'][0]['name']
+        track_data['uri'] = track['uri']
+
+        # ***** *** functional
+        track_audio_features = sp.audio_features(track['uri'])[0]
+        track_data['acousticness'] = track_audio_features['acousticness']
+        track_data['danceability'] = track_audio_features['danceability']
+        track_data['energy'] = track_audio_features['energy']
+        track_data['instrumentalness'] = track_audio_features['instrumentalness']
+        track_data['key'] = track_audio_features['key']
+        track_data['liveness'] = track_audio_features['liveness']
+        track_data['loudness'] = track_audio_features['loudness']
+        track_data['mode'] = track_audio_features['mode']
+        track_data['speechiness'] = track_audio_features['speechiness']
+        track_data['tempo'] = track_audio_features['tempo']
+        track_data['time_signature'] = track_audio_features['time_signature']
+        track_data['valence'] = track_audio_features['valence']
+
+        df = df.append(track_data, ignore_index=True)
+
+    if liked is not None:
+        df['liked'] = liked
+
+    return df
 
 
 if __name__ == "__main__":
+    # setting up
     sp = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
+
+    # playlists
     example_playlist = "29e9lxxlte7IE7dwrSHhKP"
 
-    tracks_items = get_all_playlist_items(sp, example_playlist)
-    tracks_uri = extract_tracks_uri(tracks_items)
+    # making data
+    tracks_data = make_tracks_data(sp, example_playlist, True)
+    print(tracks_data)
+    tracks_data.to_csv('out.csv')
 
-    # ***** *** NOTE - interesting to potentially get into later, but for now
+
+# ***** *** NOTE - interesting to potentially get into later, but for now
     # aa = sp.audio_analysis(tracks_uri[0])
     # print(aa['meta'])  # metadata regarding the machine analysis was run on and software
     # print(aa['track'])  # seems to have some valuable things (?)
@@ -60,4 +114,3 @@ if __name__ == "__main__":
     # print(aa['sections'])  # interesting, but rather too complicated
     # print(aa['segments'])  # as the above
     # print(aa['tatums'])  # as bars or beats
-
